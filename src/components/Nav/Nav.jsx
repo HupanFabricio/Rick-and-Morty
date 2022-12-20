e4501eceac1b9dccad4fbc1bb7d216052738f53f
Hupan <fabrihupan@gmail.com>
2022-12-20 17:11:54 -0300

    Styles Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from './Nav.module.css';
 import { Link } from "react-router-dom";
-export default function Nav (props) {
-    return(
+export default function Nav(props) {
+    return (
         <div className={styles.container}>
-            <Link to="/home">Home</Link>
-            <Link to="/about">About</Link>
+            <div className={styles.link}>
+                <Link className={styles.Link} to="/home">Home</Link>
+                <Link className={styles.Link} to="/about">About</Link>
+            </div>
             <img src="https://pngimg.com/uploads/rick_morty/rick_morty_PNG7.png" alt="skrill" />
-            <SearchBar onSearch={props.onSearch}/>
+            <SearchBar onSearch={props.onSearch} />
         </div>
     )
 }
